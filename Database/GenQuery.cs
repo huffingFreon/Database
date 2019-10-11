10b3a96de9a28eb26ab553ddefba1156f201aba9
huffingFreon <higginsba@etsu.edu>
2019-10-11 17:34:22 -0400

    Finished most of GenQuery

---
+++ Database/GenQuery.cs
@@ -0,0 +1,115 @@
+﻿using Microsoft.Data.Sqlite;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace Database
+{
+    class GenQuery<T> where T : new()
+    {
+        public SqliteConnection Connection { get; set; }
+
+        public SqliteCommand Command { get; set; }
+
+        public string TableName { get; set; }
+
+        public List<string> TableColumns { get; set; }
+
+        public GenQuery(SqliteConnection lite)
+        {
+            Connection = lite;
+            Command = new SqliteCommand();
+            Command.Connection = Connection;
+            TableName = typeof(T).Name;
+            TableColumns = new List<string>();
+
+            PropertyInfo[] propertyInfos = typeof(T).GetProperties();
+
+            for (int i = 0; i < propertyInfos.Length; i++)
+            {
+                TableColumns.Add(propertyInfos[i].Name);
+            }
+        }
+
+        public string ReadRow(int iRowID)
+        {
+            string strRow = "";
+
+            Command.CommandText = $"select * from {TableName} where {TableColumns[0]} = {iRowID}";
+
+            SqliteDataReader dataReader = Command.ExecuteReader();
+            dataReader.Read();
+
+            for (int i = 0; i < dataReader.FieldCount; i++)
+            {
+                strRow += dataReader.GetString(i) + "\t";
+            }
+
+            return strRow;
+        }
+
+        public int DeleteRow(int iRowID)
+        {
+            int iDeleted = 0;
+
+            Command.CommandText = $"delete from {TableName} where {TableColumns[0]} = {iRowID}";
+
+            iDeleted = Command.ExecuteNonQuery();
+
+            return iDeleted;
+        }
+
+        public int AddRow(T gen)
+        {
+            int iRowsAdded;
+
+            List<string> values = new List<string>();
+            
+            for (int i = 1; i < TableColumns.Count; i++)
+            {
+                values.Add(typeof(T).GetProperty(TableColumns[i]).GetValue(gen).ToString());
+            }
+
+            Command.CommandText = $"insert into {TableName} (";
+
+            for (int i = 1; i < TableColumns.Count; i++)
+            {
+                if (i < (TableColumns.Count - 1))
+                {
+                    Command.CommandText += $"{TableColumns[i]}, "; 
+                }
+                else
+                {
+                    Command.CommandText += $"{TableColumns[i]}) values ("; 
+                }
+            }
+
+            for (int i = 0; i < values.Count; i++)
+            {
+                if (i < (values.Count - 1))
+                {
+                    Command.CommandText += $"'{values[i]}', ";
+                }
+                else
+                {
+                    Command.CommandText += $"'{values[i]}')";
+                }
+            }
+
+            iRowsAdded = Command.ExecuteNonQuery();
+
+            return iRowsAdded;
+        }
+
+        public int UpdateRow(int rowID, string valueName, string value)
+        {
+            int iUpdated;
+
+            Command.CommandText = $"update {TableName} set {valueName} = '{value}' where {TableColumns[0]} = {rowID}";
+
+            iUpdated = Command.ExecuteNonQuery();
+
+            return iUpdated;
+        }
+    }
+}
---
+++ Database/Person.cs
@@ -0,0 +1,28 @@
+﻿namespace Database
+{
+    class Person
+    {
+        public int ID { get; set; }
+
+        public string First_Name { get; set; }
+
+        public string Last_Name { get; set; }
+
+        public string Job { get; set; }
+
+        public Person()
+        {
+            ID = 0;
+            First_Name = null;
+            Last_Name = null;
+            Job = null;
+        }
+
+        public Person(string firstName, string lastName, string job)
+        {
+            First_Name = firstName;
+            Last_Name = lastName;
+            Job = job;
+        }
+    }
+}
